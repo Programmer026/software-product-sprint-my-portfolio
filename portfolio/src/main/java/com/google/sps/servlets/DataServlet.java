a9f5b58b3702d269843dbe8d2d15939aed12c208
Oluwayemisi Runsewe <runseweoluwayemisi@gmail.com>
2020-06-14 23:07:06 -0500

    Add dependency and query data

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -17,6 +17,7 @@ package com.google.sps.servlets;
 import java.io.IOException;
 import com.google.gson.Gson;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -25,6 +26,9 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
@@ -34,9 +38,22 @@ public final class DataServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+      Query query = new Query("Task");
+
+      DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+      PreparedQuery results = datastore.prepare(query);
+
+        HashMap<String, String> name_comment = new HashMap<>();
+        for (Entity entity : results.asIterable()) {
+            String name = (String) entity.getProperty("Name");
+            String comment = (String) entity.getProperty("Comment");
+            name_comment.put(name, comment); 
+        }
+
+        Gson gson = new Gson();
+
     response.setContentType("application/json;");
-    String json = new Gson().toJson(quotes); 
-    response.getWriter().println(json);
+    response.getWriter().println(gson.toJson(name_comment));
   }
 
   @Override
