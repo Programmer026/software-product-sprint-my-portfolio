bad4dd59986bb0a16adee097e8068010411a72a1
Oluwayemisi Runsewe <runseweoluwayemisi@gmail.com>
2020-06-14 20:43:00 -0500

    Update function to show comments as list

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,42 +12,20 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- * Adds a random greeting to the page.
- */
- /*
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
 
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
-}
-*/
-async function getQuotesUsingAsyncAwait() {
-  const response = await fetch('/data');
-  const quote = await response.text();
-  document.getElementById('quote-container').innerText = quote;
-}
-
-function getServerStats() {
-  fetch('/data').then(response => response.json()).then((stats) => {
-    // stats is an object, not a string, so we have to
-    // reference its fields to create HTML content
-
-    const statsListElement = document.getElementById('quote-container');
-    statsListElement.innerHTML = '';
-    statsListElement.appendChild(
-        createListElement('QUOTE: ' + stats.get(0)));
+function getData() {
+  fetch('/data').then(response => response.json()).then((quote) => {
+    len = quote.length;
+  text = "<ul>";
+  for (i = 0; i < len; i++){
+      if(quote[i] === "") {continue;}
+      text += "<li>" + quote[i] + "</li>";
+  }
+  text += "</ul>";
+  document.getElementById('quote-container').innerHTML = text;
   });
 }
 
-function createListElement(text) {
-  const liElement = document.createElement('li');
-  liElement.innerText = text;
-  return liElement;
-}
+
+
+
